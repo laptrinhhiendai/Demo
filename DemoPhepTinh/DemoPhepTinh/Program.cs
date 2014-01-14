2ae569dce85487599214d58d12df485a5716b65b
tuyento <tuyento12291@gmail.com>
2014-01-14 13:40:18 +0700

    hoanthanh

---
+++ DemoPhepTinh/DemoPhepTinh/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoPhepTinh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            xinChao();
+        }
+        public static void xinChao()
+        {
+            Console.Write("Hello aaa \n");
+        }
+    }
+}
